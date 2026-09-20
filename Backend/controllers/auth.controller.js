4f7389cb75bcc2d77040f7f3b9379fed42eb1bdf
anand Thakur <satul95060@gmail.com>
2026-09-20 23:50:29 +0530

    add

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -13,11 +13,11 @@ export const googleAuth = async (req, res) => {
        
 
         let token = await genToken(user._id)
-
         res.cookie("token", token, {
-            httpOnly: true,   
+            httpOnly: true,
+            secure: true,        // ✅ add karo — sameSite: none ke saath mandatory hai
             sameSite: "none",
-            maxAge: 7 * 24 * 60 * 60 * 1000  
+            maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
         return res.status(200).json({ success: true, user })  
@@ -31,6 +31,7 @@ export const googleAuth = async (req, res) => {
 export const logout = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
+      secure: true, 
       sameSite: "none",
     });
     return res.status(200).json({ message: "Logout Successfully" });
